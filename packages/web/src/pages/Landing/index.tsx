1ea31261c45697aabd359d6c1c5207bddae74d6c
arthurgehrke <arthurgehrke07@gmail.com>
2020-08-07 09:01:31 -0300

    feat: add landing page styles

--- packages/web/src/pages/Landing/index.tsx
+++ packages/web/src/pages/Landing/index.tsx
@@ -1,25 +1,48 @@
-import React from 'react'
+import React from 'react';
 
-import logoImg from '../../assets/images/logo.svg'
-import landingImg from '../../assets/images/landing.svg'
+import logoImg from '../../assets/images/logo.svg';
+import landingImg from '../../assets/images/landing.svg';
 
-const Landing:React.FC = () => {
-  return(
+import studyIcon from '../../assets/icons/study.svg';
+import giveClassIcon from '../../assets/icons/give-classes.svg';
+import purpleHeartIcon from '../../assets/icons/purple-heart.svg';
+
+import './styles.css';
+
+const Landing: React.FC = () => {
+  return (
     <div id="landing-page">
       <div id="page-landing-content" className="container">
         <div id="logo-container">
-          <img src={logoImg} alt="Proffy"/>
+          <img src={logoImg} alt="Proffy" />
           <h2>Sua plataforma de estudos online.</h2>
         </div>
         <div>
-          <img src={landingImg} alt="Plataforma de estudos" className="hero-image"/>
+          <img
+            src={landingImg}
+            alt="Plataforma de estudos"
+            className="hero-image"
+          />
         </div>
         <div className="buttons-container">
-          <a href="" className="study"></a>
+          <a href="" className="study">
+            Estudar
+          </a>
+          <img src={studyIcon} alt="Estudar" />
+          <a href="" className="give-class">
+            Estudar
+          </a>
+
+          <img src={giveClassIcon} alt="Dar aulas" />
         </div>
+
+        <span className="total-connections">
+          Total de 200 conexões já realizadas{' '}
+          <img src={purpleHeartIcon} alt="Coração roxo" />
+        </span>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
