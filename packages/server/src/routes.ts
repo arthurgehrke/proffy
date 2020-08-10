64985f90ddda4968921a75861dd0527490e6583e
arthur gehrke <arthurgehrke07@gmail.com>
2020-08-10 06:38:55 -0700

    feat: add classes and connections controllers

--- packages/server/src/routes.ts
+++ packages/server/src/routes.ts
@@ -1,9 +1,16 @@
 import express from 'express';
 
-const routes = express.Router();
+import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
 
-routes.get('/', (request, response) =>
-  response.json({ message: 'Hello World' })
-);
+const routes = express.Router();
+const classesControllers = new ClassesController();
+const connectionsControllers = new ConnectionsController();
+
+routes.get('/classes', classesControllers.index);
+routes.post('/classes', classesControllers.create);
+
+routes.get('/connections', connectionsControllers.index);
+routes.post('/connections', connectionsControllers.create);
 
 export default routes;
